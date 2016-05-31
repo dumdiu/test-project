e32344c20b8258a4aa2649c9126c352f4f1292ef
Alex <courpron@gmail.com>
2016-05-31 10:17:07 +0200

    Adding Sabrina to the name list.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 var unique = require('unique-random-array');
 
-var tab = ["Alex", "Marc", "Vincent", "Pierre", "Paul", "Jacques"];
+var tab = ["Alex", "Marc", "Vincent", "Pierre", "Paul", "Jacques", "Sabrina"];
 module.exports = {
   hello: function () {
     console.log("hello " + unique(tab)());
