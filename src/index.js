563e8513033fd66d90bd0a3b52c1dc469cd42228
Alex <courpron@gmail.com>
2016-06-01 19:58:35 +0200

    feat(name array): Add John

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 var unique = require('unique-random-array');
 
-var tab = ["Alex", "Marc", "Vincent", "Pierre", "Paul", "Jacques", "Sabrina"];
+var tab = ["Alex", "Marc", "Vincent", "Pierre", "Paul", "Jacques", "Sabrina", "John"];
 module.exports = {
   hello: function () {
     console.log("hello " + unique(tab)());
